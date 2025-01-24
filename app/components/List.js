8cb67144e53d0df047771931dbc094355c8b799f
Clark Zivelonghi <CZivelonghi@CBIZ.com>
2025-01-24 09:21:33 -0500

    Added ability to create a new token.

--- app/components/List.js
+++ app/components/List.js
@@ -2,9 +2,30 @@ import { ethers } from "ethers"
 
 function List({ toggleCreate, fee, provider, factory }) {
 
+  async function listHandler(form) {
+    const name = form.get("name")
+    const symbol = form.get("symbol")
+    const signer = await provider.getSigner()
+
+    const tx = await factory.connect(signer).create(name, symbol, {value: fee})
+    const receipt = await tx.wait()
+
+    toggleCreate()
+  }
+
   return (
     <div className="list">
-
+      <h2>list new token</h2>
+      <div className="list_description">
+        <p>fee: {ethers.formatEther(fee)}</p>
+      </div>
+      
+      <form action={listHandler}>
+        <input type="text" name="name" placeholder="name" />
+        <input type="text" name="symbol" placeholder="symbol" />
+        <input type="submit" value="[ list ]" />
+      </form>
+      <button onClick={() => toggleCreate()} className="btn--fancy">[cancel]</button>
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -15,12 +15,55 @@ import config from "./config.json"
 import images from "./images.json"
 
 export default function Home() {
+  const [provider, setProvider] = useState(null)
+  const [account, setAccount] = useState(null)
+  const [factory, setFactory] = useState(null)
+  const [fee, setFee] = useState(0)
+  const [showCreate, setShowCreate] = useState(false)
+
+  function toggleCreate() {
+    setShowCreate(!showCreate)
+  }
+
+  async function loadBlockChainData() {
+    const provider = new ethers.BrowserProvider(window.ethereum)
+    setProvider(provider)
+
+    const network = await provider.getNetwork()
+
+    const factory = new ethers.Contract( config[network.chainId].factory.address, Factory, provider)
+    setFactory(factory)
+
+    const fee = await factory.fee()
+    setFee(fee)
+  }
+
+  useEffect(() => {
+    loadBlockChainData()
+  }, [])
 
   return (
     <div className="page">
+      <Header account={account} setAccount={setAccount}></Header>
+      
+      <main>
+        <div className="create"> 
+          <button onClick={factory && account &&toggleCreate} className="btn--fancy">
+            { !factory ?  (
+              "[ contract not deployed ]"
+            ) : !account ? (
+              "[ connect wallet ]"
+            ) : (
+              "[ create a new token ]"
+            )}
+          </button>
+        </div>
 
-      <h1 style={{ padding: "1em" }}>fun.pump</h1>
-
+        {showCreate && (
+          <List toggleCreate={toggleCreate} fee={fee} provider={provider} factory={factory}></List>
+        )}
+        
+      </main>
     </div>
   );
 }
